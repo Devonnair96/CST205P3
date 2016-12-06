0bba3c4aa9005ed4b9d81a777d7954498f6fe58e
long3686 <jarlong@csumb.edu>
2016-12-05 16:27:55 -0800

    Gold system

--- TextGame.py
+++ TextGame.py
@@ -3,8 +3,15 @@ import time
 from tkinter import messagebox
 import random
 import smtplib
+import getpass
 from PIL import ImageTk, Image
-
+gold = 500
+def resetGold():
+    spentGold()
+    reset()
+def spentGold():
+    global gold
+    gold = gold - 250
 def ClearScreen():
     button1.destroy()
     button2.destroy()
@@ -15,6 +22,7 @@ def ClearScreen():
     textLabel.destroy()
     entry.destroy()
 def reset():
+    goldLabel.destroy()
     frame1.pack_forget()
     if (choice == 1):
         WizardCreateDoors(numberOfDoors2)
@@ -36,17 +44,22 @@ def createRooms(img1, img2):
     label2 = Label(frame1,image= photoImage2)
     label2.image = photoImage2
     label2.pack(side = RIGHT)
-    button1 = Button(frame1, command = reset)
-    button1.pack(side = RIGHT)
+    if gold >= 250:
+        button1 = Button(frame1, command = resetGold)
+        button1.pack(side = RIGHT)
     button2 = Button(frame1, command = reset)
     button2.pack(side = RIGHT)
+    costlabel = Label(frame1, text = "Gold Price: 0")
+    costlabel.pack(side = LEFT)
+    costlabel2 = Label(frame1, text = "Gold Price: 250")
+    costlabel2.pack(side = RIGHT)
 
 def staffRoom():
-    createRooms("firestaff1.jpg", "WaterStaff1.jpg")
+    createRooms("waterstaff1.jpg", "fireStaff1.jpg")
 def topRoom():
     createRooms("firetop1.jpg", "waterRobe1.jpg")
 def wizardHatRoom():
-    createRooms("waterHat1.jpg", "FireHat1.jpg")
+    createRooms("fireHat1.jpg", "waterHat1.jpg")
 def bottomRoom():
     createRooms("fireRobe1.jpg", "waterBottom1.jpg")
 
@@ -69,7 +82,7 @@ def torsoRoom():
     createRooms("elementalArcherTop.png", "ArcherTop.jpg")
 
 randomArray = [1,2,3,4]
-
+randomGold = [1,2,3,4]
 
 def WizardDoorUpdate():
     numberOfDoors1 = numberOfTimesDecremented
@@ -77,26 +90,42 @@ def WizardDoorUpdate():
     global numberOfDoors2
     numberOfDoors2 = numberOfDoors1  - 1
 
-
     randomNumber = random.choice(randomArray)
+    goldFind = random.choice(randomGold)
 
     if randomNumber == 1:
         messagebox.showinfo("You have entered the staff room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         staffRoom()
     if randomNumber == 2:
         messagebox.showinfo("You have entered the top room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         topRoom()
     if randomNumber == 3:
         messagebox.showinfo("You have entered the bottom room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         bottomRoom()
     if randomNumber == 4:
         messagebox.showinfo("You have entered the hat room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         wizardHatRoom()
@@ -107,24 +136,40 @@ def WarriorDoorUpdate():
     global numberOfDoors2
     numberOfDoors2 = numberOfDoors1  - 1
     randomNumber = random.choice(randomArray)
-
+    goldFind = random.choice(randomGold)
     if randomNumber == 1:
         messagebox.showinfo("You have entered the sword room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         swordRoom()
     if randomNumber == 2:
         messagebox.showinfo("You have entered the chest room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         chestRoom()
     if randomNumber == 3:
         messagebox.showinfo("You have entered the greaves room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         legsRoom()
     if randomNumber == 4:
         messagebox.showinfo("You have entered the helm room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         helmRoom()
@@ -135,23 +180,40 @@ def RangerDoorUpdate():
     global numberOfDoors2
     numberOfDoors2 = numberOfDoors1  - 1
     randomNumber = random.choice(randomArray)
+    goldFind = random.choice(randomGold)
     if randomNumber == 1:
         messagebox.showinfo("You have entered the bow room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         bowRoom()
     if randomNumber == 2:
         messagebox.showinfo("You have entered the torso room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         torsoRoom()
     if randomNumber == 3:
         messagebox.showinfo("You have entered the leggings room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         leggingsRoom()
     if randomNumber == 4:
         messagebox.showinfo("You have entered the hat room")
+        if goldFind == 1:
+            global gold
+            messagebox.showinfo("You also found 250 gold in the room")
+            gold = gold + 250
         frame.pack_forget()
         randomArray.remove(randomNumber)
         RangerHatRoom()
@@ -159,16 +221,17 @@ def RangerDoorUpdate():
 def WizardDoor():
     global userEmail
     userEmail = entry.get()
-    try:
-        gameserver  =  smtplib.SMTP('smtp.gmail.com',587)
-        gameserver.ehlo()
-    except:
-        print ("There was a problem")
-    gameserver.starttls()
-    password = getpass.getpass("Enter your password: ")
-    gameserver.login( userEmail, password) # the user sends the results to themselves
-    gameserver.sendmail( userEmail, userEmail, "Hello player") #placeholder until results are ready
-    gameserver.quit()
+    if userEmail != "":
+        try:
+            gameserver  =  smtplib.SMTP('smtp.gmail.com',587)
+            gameserver.ehlo()
+        except:
+            print ("There was a problem")
+        gameserver.starttls()
+        password = getpass.getpass("Enter your password: ")
+        gameserver.login( userEmail, password) # the user sends the results to themselves
+        gameserver.sendmail( userEmail, userEmail, "Hello player") #placeholder until results are ready
+        gameserver.quit()
     ClearScreen()
     global choice
     choice = 1
@@ -177,16 +240,17 @@ def WizardDoor():
 def WarriorDoor():
     global userEmail
     userEmail = entry.get()
-    try:
-        gameserver  =  smtplib.SMTP('smtp.gmail.com',587)
-        gameserver.ehlo()
-    except:
-        print("There was a problem")
-    gameserver.starttls()
-    password = getpass.getpass("Enter your password: ")
-    gameserver.login( userEmail, password) # the user sends the results to themselves
-    gameserver.sendmail( userEmail, userEmail, "Hello player") #placeholder until results are ready
-    gameserver.quit()
+    if userEmail != "":
+        try:
+            gameserver  =  smtplib.SMTP('smtp.gmail.com',587)
+            gameserver.ehlo()
+        except:
+            print("There was a problem")
+            gameserver.starttls()
+            password = getpass.getpass("Enter your password: ")
+            gameserver.login( userEmail, password) # the user sends the results to themselves
+            gameserver.sendmail( userEmail, userEmail, "Hello player") #placeholder until results are ready
+            gameserver.quit()
     ClearScreen()
     global choice
     choice = 2
@@ -195,16 +259,17 @@ def WarriorDoor():
 def RangerDoor():
     global userEmail
     userEmail = entry.get()
-    try:
-        gameserver  =  smtplib.SMTP('smtp.gmail.com',587)
-        gameserver.ehlo()
-    except:
-        print("There was a problem")
-    gameserver.starttls()
-    password = getpass.getpass("Enter your password: ")
-    gameserver.login( userEmail, password) # the user sends the results to themselves
-    gameserver.sendmail( userEmail, userEmail, "Hello player") #placeholder until results are ready
-    gameserver.quit()
+    if userEmail != "":
+        try:
+            gameserver  =  smtplib.SMTP('smtp.gmail.com',587)
+            gameserver.ehlo()
+        except:
+            print("There was a problem")
+            gameserver.starttls()
+            password = getpass.getpass("Enter your password: ")
+            gameserver.login( userEmail, password) # the user sends the results to themselves
+            gameserver.sendmail( userEmail, userEmail, "Hello player") #placeholder until results are ready
+            gameserver.quit()
     ClearScreen()
     global choice
     choice = 3
@@ -212,6 +277,11 @@ def RangerDoor():
     RangerCreateDoors(4)
 
 def WizardCreateDoors(numberOfDoors):
+    goldamount = StringVar()
+    goldamount.set("Gold: " + str(gold))
+    global goldLabel
+    goldLabel = Label(newWindow, textvariable = goldamount)
+    goldLabel.pack(side = TOP)
     global numberOfTimesDecremented
     numberOfTimesDecremented = 0
     img5 = Image.open("door5.jpg")
@@ -229,6 +299,11 @@ def WizardCreateDoors(numberOfDoors):
         numberOfDoors = numberOfDoors - 1
         numberOfTimesDecremented = numberOfTimesDecremented + 1
 def WarriorCreateDoors(numberOfDoors):
+    goldamount = StringVar()
+    goldamount.set("Gold: " + str(gold))
+    global goldLabel
+    goldLabel = Label(newWindow, textvariable = goldamount)
+    goldLabel.pack(side = TOP)
     global numberOfTimesDecremented
     numberOfTimesDecremented = 0
     img5 = Image.open("door5.jpg")
@@ -246,6 +321,11 @@ def WarriorCreateDoors(numberOfDoors):
         numberOfDoors = numberOfDoors - 1
         numberOfTimesDecremented = numberOfTimesDecremented + 1
 def RangerCreateDoors(numberOfDoors):
+    goldamount = StringVar()
+    goldamount.set("Gold: " + str(gold))
+    global goldLabel
+    goldLabel = Label(newWindow, textvariable = goldamount)
+    goldLabel.pack(side = TOP)
     global numberOfTimesDecremented
     numberOfTimesDecremented = 0
     img5 = Image.open("door5.jpg")
@@ -265,17 +345,27 @@ def RangerCreateDoors(numberOfDoors):
 
 newWindow = Tk()
 mytext = StringVar()
+goldamount = StringVar()
+goldamount.set("Gold: " + str(gold))
+global goldLabel
+goldLabel = Label(newWindow, textvariable = goldamount)
 entry = Entry(newWindow, textvariable = mytext)
 entry.pack(side = BOTTOM)
 textLabel = Label(newWindow, text = "Enter your email")
 textLabel.pack(side = BOTTOM)
 img1 = Image.open("door5.jpg")
 img2 = ImageTk.PhotoImage(img1)
-label1 = Label(newWindow, image = img2)
+warriorImg = Image.open("fighter.jpg")
+warriorImg2 = ImageTk.PhotoImage(warriorImg)
+wizardImg = Image.open("wizard.png")
+wizardImg2 = ImageTk.PhotoImage(wizardImg)
+rangerImg = Image.open("ranger.jpg")
+rangerImg2 = ImageTk.PhotoImage(rangerImg)
+label1 = Label(newWindow, image = wizardImg2)
 label1.pack(side = RIGHT)
-label2 = Label(newWindow, image = img2)
+label2 = Label(newWindow, image = warriorImg2)
 label2.pack(side = RIGHT)
-label3 = Label(newWindow, image = img2)
+label3 = Label(newWindow, image = rangerImg2)
 label3.pack(side = RIGHT)
 wizardDoorCount = 4
 button1 = Button(newWindow, text = "WIZARD", command = WizardDoor)
